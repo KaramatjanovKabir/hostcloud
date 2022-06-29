d1f5f6447f2b0b9e51e4d241826e990176293a7c
KaramatjanovKabir <kabir_karamatjanov@mail.ru>
2022-06-29 18:43:01 +0500

    hostcloud finished

---
+++ app.js
@@ -0,0 +1,11 @@
+const burger = document.getElementById("burger");
+const Nav = document.querySelector(".nav_mobile");
+
+ burger.addEventListener("click", function() {
+    Nav.classList.toggle ("show-menu")
+});
+
+    
+
+
+
